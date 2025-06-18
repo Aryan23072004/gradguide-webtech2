f52561a162e24aca28b82009c60174c0e5c716bf
Aryan Kulkarni <aryan23072004@gmail.com>
2025-06-18 14:59:38 +0300

    Final project setup, updated README, layout fixes, dashboards ready

--- resources/views/dashboards/student.blade.php
+++ resources/views/dashboards/student.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-2xl font-bold">Welcome, {{ Auth::user()->name }}!</h1>
+    <h1 class="text-2xl font-bold">Welcome, {{ Auth::user()->name }}</h1>
     <p class="mt-2">You are logged in as <strong>{{ Auth::user()->role }}</strong>.</p>
 @endsection
