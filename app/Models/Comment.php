35764c2136f8667ebf523f15fcb270f449cceb84
Aryan Kulkarni <aryan23072004@gmail.com>
2025-06-21 18:10:03 +0300

    testing2

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,6 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
-    ## Test coment added to check git connection
-    protected $fillable = ['user_id', 'post_id', 'content'];
 }
